2df8b496ee0762c30040bae86b198633a8609e64
Danny <danny.alvarado.monge@est.una.ac.cr>
2026-05-13 20:14:27 -0600

    Lineas de comentarios

--- src/app/api/students/route.js
+++ src/app/api/students/route.js
@@ -1,5 +1,6 @@
 // Establecemos la API para conectar con frontend
 // Importamos los metodos CRUD realizados en Service
+//Aqui esta la logica de MySQL
 import { createStudents } from "@/services/studentService";
 import { getStudents } from "@/services/studentService";
 import { updateStudent } from "@/services/studentService";
